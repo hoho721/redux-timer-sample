c2a7c85579142f4e0e9bf4c02ce97e7fbff9da3b
hoho721 <sjmsong@gmail.com>
2019-05-02 18:04:38 +0900

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Timer from './Components/Timer';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Timer />
+    );
+  }
+}
